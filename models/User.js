163c2b185e93e31808e5b239aa737415bee0b265
Goutham Reddy <gouthamvreddy@gmail.com>
2015-07-25 18:17:05 -0700

    updated User model

--- models/User.js
+++ models/User.js
@@ -3,7 +3,8 @@
 var mongoose = require('mongoose');
 
 var userSchema = mongoose.Schema({
-  username: String
+  username: String,
+  files: Array
 });
 
 module.exports = mongoose.model('User', userSchema);
